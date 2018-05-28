387b49751bf3afbaa487f58c4b454cae9aa136c9
answes <jlbs@163.com>
2018-05-28 10:07:11 +0800

    界面修改

--- src/main/java/bean/Goods.java
+++ src/main/java/bean/Goods.java
@@ -1,5 +1,7 @@
 package bean;
 
+import javafx.beans.property.*;
+
 /**
  * @Author : bigshark
  * @Descripstion :
@@ -7,4 +9,347 @@ package bean;
  * @Modificd :
  */
 public class Goods {
+    private IntegerProperty number;
+    private StringProperty commNum;
+    private StringProperty commName;
+    private DoubleProperty openPrice;
+    private DoubleProperty newPrice;
+    private IntegerProperty count;
+    private DoubleProperty upDown;
+    private DoubleProperty extent;  //  幅度
+    private DoubleProperty maxPrice;
+    private DoubleProperty mixPrice;
+    private DoubleProperty yestedayPrice;
+    private DoubleProperty comePrice;
+    private IntegerProperty comeNumber;
+    private DoubleProperty outPrice;
+    private IntegerProperty outNumber;
+    private IntegerProperty overNumber;
+    private DoubleProperty overMoney;
+    private DoubleProperty avgPrice;
+    private IntegerProperty stockNumber;
+    private DoubleProperty numberScale;
+    private DoubleProperty trustScale;
+    private DoubleProperty exchangeScale;
+
+    public Goods() {
+
+    }
+
+    public Goods(int number, String commNum, String commName, double openPrice, double newPrice, int count, double upDown, double extent, double maxPrice, double mixPrice, double yestedayPrice, double comePrice, int comeNumber, double outPrice, int outNumber, int overNumber, double overMoney, double avgPrice, int stockNumber, double numberScale, double trustScale, double exchangeScale) {
+        this.number =new SimpleIntegerProperty(number);
+        this.commNum =new SimpleStringProperty(commNum) ;
+        this.commName =new SimpleStringProperty( commName);
+        this.openPrice =new SimpleDoubleProperty( openPrice);
+        this.newPrice =new SimpleDoubleProperty( newPrice);
+        this.count = new SimpleIntegerProperty(count);
+        this.upDown = new SimpleDoubleProperty(upDown);
+        this.extent = new SimpleDoubleProperty(extent);
+        this.maxPrice =new SimpleDoubleProperty( maxPrice);
+        this.mixPrice = new SimpleDoubleProperty(mixPrice);
+        this.yestedayPrice = new SimpleDoubleProperty(yestedayPrice);
+        this.comePrice =new SimpleDoubleProperty( comePrice);
+        this.comeNumber =new SimpleIntegerProperty( comeNumber);
+        this.outPrice =new SimpleDoubleProperty( outPrice);
+        this.outNumber = new SimpleIntegerProperty(outNumber);
+        this.overNumber = new SimpleIntegerProperty(overNumber);
+        this.overMoney = new SimpleDoubleProperty(overMoney);
+        this.avgPrice = new SimpleDoubleProperty(avgPrice);
+        this.stockNumber = new SimpleIntegerProperty(stockNumber);
+        this.numberScale = new SimpleDoubleProperty(numberScale);
+        this.trustScale =new SimpleDoubleProperty( trustScale);
+        this.exchangeScale =new SimpleDoubleProperty( exchangeScale);
+    }
+
+    public int getNumber() {
+        return number.get();
+    }
+
+    public IntegerProperty numberProperty() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number.set(number);
+    }
+
+    public String getCommNum() {
+        return commNum.get();
+    }
+
+    public StringProperty commNumProperty() {
+        return commNum;
+    }
+
+    public void setCommNum(String commNum) {
+        this.commNum.set(commNum);
+    }
+
+    public String getCommName() {
+        return commName.get();
+    }
+
+    public StringProperty commNameProperty() {
+        return commName;
+    }
+
+    public void setCommName(String commName) {
+        this.commName.set(commName);
+    }
+
+    public double getOpenPrice() {
+        return openPrice.get();
+    }
+
+    public DoubleProperty openPriceProperty() {
+        return openPrice;
+    }
+
+    public void setOpenPrice(double openPrice) {
+        this.openPrice.set(openPrice);
+    }
+
+    public double getNewPrice() {
+        return newPrice.get();
+    }
+
+    public DoubleProperty newPriceProperty() {
+        return newPrice;
+    }
+
+    public void setNewPrice(double newPrice) {
+        this.newPrice.set(newPrice);
+    }
+
+    public int getCount() {
+        return count.get();
+    }
+
+    public IntegerProperty countProperty() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count.set(count);
+    }
+
+    public double getUpDown() {
+        return upDown.get();
+    }
+
+    public DoubleProperty upDownProperty() {
+        return upDown;
+    }
+
+    public void setUpDown(double upDown) {
+        this.upDown.set(upDown);
+    }
+
+    public double getExtent() {
+        return extent.get();
+    }
+
+    public DoubleProperty extentProperty() {
+        return extent;
+    }
+
+    public void setExtent(double extent) {
+        this.extent.set(extent);
+    }
+
+    public double getMaxPrice() {
+        return maxPrice.get();
+    }
+
+    public DoubleProperty maxPriceProperty() {
+        return maxPrice;
+    }
+
+    public void setMaxPrice(double maxPrice) {
+        this.maxPrice.set(maxPrice);
+    }
+
+    public double getMixPrice() {
+        return mixPrice.get();
+    }
+
+    public DoubleProperty mixPriceProperty() {
+        return mixPrice;
+    }
+
+    public void setMixPrice(double mixPrice) {
+        this.mixPrice.set(mixPrice);
+    }
+
+    public double getYestedayPrice() {
+        return yestedayPrice.get();
+    }
+
+    public DoubleProperty yestedayPriceProperty() {
+        return yestedayPrice;
+    }
+
+    public void setYestedayPrice(double yestedayPrice) {
+        this.yestedayPrice.set(yestedayPrice);
+    }
+
+    public double getComePrice() {
+        return comePrice.get();
+    }
+
+    public DoubleProperty comePriceProperty() {
+        return comePrice;
+    }
+
+    public void setComePrice(double comePrice) {
+        this.comePrice.set(comePrice);
+    }
+
+    public int getComeNumber() {
+        return comeNumber.get();
+    }
+
+    public IntegerProperty comeNumberProperty() {
+        return comeNumber;
+    }
+
+    public void setComeNumber(int comeNumber) {
+        this.comeNumber.set(comeNumber);
+    }
+
+    public double getOutPrice() {
+        return outPrice.get();
+    }
+
+    public DoubleProperty outPriceProperty() {
+        return outPrice;
+    }
+
+    public void setOutPrice(double outPrice) {
+        this.outPrice.set(outPrice);
+    }
+
+    public int getOutNumber() {
+        return outNumber.get();
+    }
+
+    public IntegerProperty outNumberProperty() {
+        return outNumber;
+    }
+
+    public void setOutNumber(int outNumber) {
+        this.outNumber.set(outNumber);
+    }
+
+    public int getOverNumber() {
+        return overNumber.get();
+    }
+
+    public IntegerProperty overNumberProperty() {
+        return overNumber;
+    }
+
+    public void setOverNumber(int overNumber) {
+        this.overNumber.set(overNumber);
+    }
+
+    public double getOverMoney() {
+        return overMoney.get();
+    }
+
+    public DoubleProperty overMoneyProperty() {
+        return overMoney;
+    }
+
+    public void setOverMoney(double overMoney) {
+        this.overMoney.set(overMoney);
+    }
+
+    public double getAvgPrice() {
+        return avgPrice.get();
+    }
+
+    public DoubleProperty avgPriceProperty() {
+        return avgPrice;
+    }
+
+    public void setAvgPrice(double avgPrice) {
+        this.avgPrice.set(avgPrice);
+    }
+
+    public int getStockNumber() {
+        return stockNumber.get();
+    }
+
+    public IntegerProperty stockNumberProperty() {
+        return stockNumber;
+    }
+
+    public void setStockNumber(int stockNumber) {
+        this.stockNumber.set(stockNumber);
+    }
+
+    public double getNumberScale() {
+        return numberScale.get();
+    }
+
+    public DoubleProperty numberScaleProperty() {
+        return numberScale;
+    }
+
+    public void setNumberScale(double numberScale) {
+        this.numberScale.set(numberScale);
+    }
+
+    public double getTrustScale() {
+        return trustScale.get();
+    }
+
+    public DoubleProperty trustScaleProperty() {
+        return trustScale;
+    }
+
+    public void setTrustScale(double trustScale) {
+        this.trustScale.set(trustScale);
+    }
+
+    public double getExchangeScale() {
+        return exchangeScale.get();
+    }
+
+    public DoubleProperty exchangeScaleProperty() {
+        return exchangeScale;
+    }
+
+    public void setExchangeScale(double exchangeScale) {
+        this.exchangeScale.set(exchangeScale);
+    }
+
+    @Override
+    public String toString() {
+        return "Commodity{" +
+                "number=" + number +
+                ", commNum='" + commNum + '\'' +
+                ", commName='" + commName + '\'' +
+                ", openPrice=" + openPrice +
+                ", newPrice=" + newPrice +
+                ", count=" + count +
+                ", upDown='" + upDown + '\'' +
+                ", extent='" + extent + '\'' +
+                ", maxPrice=" + maxPrice +
+                ", mixPrice=" + mixPrice +
+                ", yestedayPrice=" + yestedayPrice +
+                ", comePrice=" + comePrice +
+                ", comeNumber=" + comeNumber +
+                ", outPrice=" + outPrice +
+                ", outNumber=" + outNumber +
+                ", overNumber=" + overNumber +
+                ", overMoney=" + overMoney +
+                ", avgPrice=" + avgPrice +
+                ", stockNumber=" + stockNumber +
+                ", numberScale=" + numberScale +
+                ", trustScale=" + trustScale +
+                ", exchangeScale=" + exchangeScale +
+                '}';
+    }
 }
--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -8,6 +8,8 @@ import chart.HQApplet;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingNode;
 import javafx.event.*;
 import javafx.fxml.FXML;
@@ -17,6 +19,7 @@ import javafx.geometry.Bounds;
 import javafx.scene.control.*;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -51,6 +54,35 @@ public class MainController implements ControlledStage ,Initializable{
     private AnchorPane goods_root;
     @FXML
     private TableView<Goods> tb_goods;
+    /**
+     * tableview Column
+     */
+    @FXML
+    private TableColumn<Goods, String> commNum;
+    @FXML
+    private TableColumn<Goods, String> commName;
+    @FXML
+    private TableColumn<Goods, String> openPrice;
+    @FXML
+    private TableColumn<Goods, String> newPrice;
+    @FXML
+    private TableColumn<Goods, String> upDown;
+    @FXML
+    private TableColumn<Goods, String> extent;
+    @FXML
+    private TableColumn<Goods, String> maxPrice;
+    @FXML
+    private TableColumn<Goods, String> mixPrice;
+    @FXML
+    private TableColumn<Goods, String> yestedayPrice;
+    @FXML
+    private TableColumn<Goods, String> comePrice;
+    @FXML
+    private TableColumn<Goods, String> outPrice;
+    @FXML
+    private TableColumn<Goods, String> overNumber;
+    @FXML
+    private TableColumn<Goods, String> overMoney;
 
     @FXML
     private Label timeNow;
@@ -58,13 +90,6 @@ public class MainController implements ControlledStage ,Initializable{
     @FXML
     private javafx.scene.control.Button to_transaction;
 
-    private StageController myController;
-    private NewsController newsController;
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        init();
-    }
     private double xOffset = 0;
     private double yOffset = 0;
     private Date oldDate=new Date();
@@ -83,6 +108,80 @@ public class MainController implements ControlledStage ,Initializable{
     int height ;
     @FXML
     Pane kline;
+
+    private StageController myController;
+    private NewsController newsController;
+    private final ObservableList<Goods> datas = FXCollections.observableArrayList(new Goods(1, "80008", "人参糖", 12.0, 10.3, 30, 0.12, 0.03, 35.0, 9.5, 20.3, 25.5, 56, 25.6, 33, 80, 23.1, 22.3, 10000, 0.00, 0.00, 0.00)
+            , new Goods(2, "80008", "芝麻糖", 12.0, 10.3, 20, 1.12, 0.03, 35.0, 9.5, 20.3, 25.5, 56, 25.6, 33, 80, 23.1, 22.3, 10000, 0.00, 0.00, 0.00)
+            , new Goods(3, "80009", "百度糖", 22.0, 20.3, 30, 2.12, 0.03, 35.0, 9.5, 20.3, 25.5, 56, 25.6, 33, 80, 23.1, 22.3, 10000, 0.00, 0.00, 0.00)
+            , new Goods(4, "80010", "不知糖", 32.0, 30.3, 40, 3.12, 0.03, 35.0, 9.5, 20.3, 25.5, 56, 25.6, 33, 80, 23.1, 22.3, 10000, 0.00, 0.00, 0.00)
+            , new Goods(5, "80011", "砂糖糖", 42.0, 40.3, 50, 4.12, 0.03, 35.0, 9.5, 20.3, 25.5, 56, 25.6, 33, 80, 23.1, 22.3, 10000, 0.00, 0.00, 0.00)
+            , new Goods(6, "80012", "人参糖", 52.0, 50.3, 60, 5.12, 0.03, 35.0, 9.5, 20.3, 25.5, 56, 25.6, 33, 80, 23.1, 22.3, 10000, 0.00, 0.00, 0.00));
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        init();
+        initGood();
+        kline.setVisible(false);
+
+    }
+
+    private void initGood() {
+        tb_goods.setItems(datas);
+        commNum.setCellValueFactory(data -> data.getValue().commNumProperty());
+        commName.setCellValueFactory(data -> data.getValue().commNameProperty());
+        openPrice.setCellValueFactory(data -> data.getValue().openPriceProperty().asString());
+        newPrice.setCellValueFactory(data -> data.getValue().newPriceProperty().asString());
+        upDown.setCellValueFactory(data -> data.getValue().upDownProperty().asString());
+        extent.setCellValueFactory(data -> data.getValue().extentProperty().asString());
+        maxPrice.setCellValueFactory(data -> data.getValue().maxPriceProperty().asString());
+        mixPrice.setCellValueFactory(data -> data.getValue().mixPriceProperty().asString());
+        yestedayPrice.setCellValueFactory(data -> data.getValue().yestedayPriceProperty().asString());
+        comePrice.setCellValueFactory(data -> data.getValue().comePriceProperty().asString());
+        outPrice.setCellValueFactory(data -> data.getValue().outPriceProperty().asString());
+        overNumber.setCellValueFactory(data -> data.getValue().overNumberProperty().asString());
+        overMoney.setCellValueFactory(data -> data.getValue().overMoneyProperty().asString());
+
+        tb_goods.setRowFactory(tv -> {
+            TableRow<Goods> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY) {
+                    Goods Goods = row.getItem();
+                    if (event.getClickCount() == 2) {
+                        setRowOnTwoClick(Goods);
+                    } else if (event.getClickCount() == 1) {
+                        setRowOnOneClick(Goods);
+                    }
+
+                }
+            });
+            return row;
+        });
+
+    }
+
+
+    /**
+     * 单击设置数据到买入，卖出中
+     *
+     * @param commodity
+     */
+    private void setRowOnOneClick(Goods commodity) {
+
+    }
+
+    /**
+     * 双击进入分时图
+     *
+     * @param commodity
+     */
+    private void setRowOnTwoClick(Goods commodity) {
+        goods_root.setVisible(false);
+        tb_goods.setVisible(false);
+        kline.setVisible(true);
+    }
+
     private void init() {
         //记录窗口位置
         top_title.setOnMousePressed(event -> {
@@ -316,5 +415,11 @@ public class MainController implements ControlledStage ,Initializable{
     }
 
     public void toTranstion(ActionEvent event) {
+    }
+
+    public void back(ActionEvent event) {
+        goods_root.setVisible(true);
+        tb_goods.setVisible(true);
+        kline.setVisible(false);
     }
 }
