295ff036126531f0a48b9d6a8f9351a4c60dbdc4
answes <jlbs@163.com>
2018-05-30 17:22:42 +0800

    界

--- src/main/java/controller/center/NewCenterController.java
+++ src/main/java/controller/center/NewCenterController.java
@@ -9,6 +9,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 
@@ -27,7 +28,15 @@ public class NewCenterController implements  Initializable {
     @FXML
     private ListView<News> lv_news;
     @FXML
+    private Label new1,new2,new3,new4,new5,new6,new7,new8;
+    @FXML
     private NewsPupWindow newsPupWindow;
+
+    private String hover = "-fx-background-color: transparent;" +
+            "-fx-text-fill:white;" +
+            "-fx-padding: 0 0 0 20px;" +
+            "-fx-graphic-text-gap:10px;"+"-fx-";
+    private String hide = "";
 
     ObservableList<News> list = FXCollections.observableArrayList(new News(1L,"2018-05-05 00:00:00","一带一路”政策密集出台 今年有望纵深推进"),new News(2L,"2018-05-05 00:00:00","一带一路”政策密集出台 今年有望纵深推进")
     ,new News(3L,"2018-05-05 00:00:00","一带一路”政策密集出台 今年有望纵深推进"),new News(4L,"2018-05-05 00:00:00","一带一路”政策密集出台 今年有望纵深推进"),new News(5L,"2018-05-05 00:00:00","一带一路”政策密集出台 今年有望纵深推进"));
@@ -44,11 +53,6 @@ public class NewCenterController implements  Initializable {
                 (ObservableValue<? extends News> ov, News old_val,
                  News new_val) -> {
                     newsPupWindow = new NewsPupWindow(list,new_val);
-//                    if(newsPupWindow!=null){
-//                        newsPupWindow.setData(new_val);
-//                    }else{
-//
-//                    }
                 });
     }
 
